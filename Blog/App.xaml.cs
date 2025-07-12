aa1684287c05c7cf44e67f38a5e0b1f58ad4ac43
Alessandro R Rey <alessandro.rioboo@gmail.com>
2025-07-12 09:11:02 -0300

    Adicionar arquivos de projeto.

---
+++ Blog/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Blog
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Blog/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Blog
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
